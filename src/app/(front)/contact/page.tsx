f26dc643c64f6fe6e49dd5b4e3d50df69011eb5f
pathompornyanglang <pathomporny@room-innovation.com>
2026-08-25 14:23:08 +0700

    update

--- src/app/(front)/contact/page.tsx
+++ src/app/(front)/contact/page.tsx
@@ -10,9 +10,7 @@ import {
 } from "lucide-react";
 import ContactForm from "./contact-form";
 
-// TODO: Cache Components adoption. Refactor this route so this opt-out can be removed.
-// See: https://nextjs.org/docs/app/guides/migrating-to-cache-components
-export const instant = false;
+export const revalidate = 3600;
 
 const contactInfo = [
   {
